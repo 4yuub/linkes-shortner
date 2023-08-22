aa515e061b4a84737a475f4487fba8246fe22704
4yuub <karafi.work@gmail.com>
2023-08-22 19:00:21 +0100

    use redis as a database

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import json
+import os
 import re
 from random import choices
 from string import ascii_letters, digits
 
+import redis
+from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, url_for
+
+load_dotenv()
 
 app = Flask(__name__)
 
-app_data = json.load(open('data.json'))
+db = redis.from_url(os.environ.get('REDIS_URL'))
+db.ping()
+
 LINK_REGEX = re.compile(
     r"^(https?://)([a-zA-Z0-9_-]+\.)+[a-zA-Z]{2,6}(/.*)?$")
 
 
 def get_random_unique_key():
     key = ''.join(choices(ascii_letters + digits, k=6))
-    while key in app_data:
+    while db.get("shortner_" + key) is not None:
         key = ''.join(choices(ascii_letters + digits, k=6))
     return key
 
@@ -27,7 +34,7 @@ def index():
 @app.route('/<key>/password', methods=['POST'])
 def check_password(key):
     body = request.get_json()
-    data = app_data.get(key)
+    data = json.loads(db.get("shortner_" + key))
 
     if data is None:
         return {'message': 'Invalid URL.'}, 400
@@ -35,13 +42,12 @@ def check_password(key):
     if body.get('password') != data.get('password'):
         return {'message': 'Invalid Password.'}, 400
 
-    print(data['originalUrl'])
     return {'originalUrl': data['originalUrl']}
 
 
 @app.route('/<key>')
 def redirect_to_original_url(key):
-    data = app_data.get(key)
+    data = json.loads(db.get("shortner_" + key))
     if data is None:
         return redirect(url_for('index'))
 
@@ -52,7 +58,6 @@ def redirect_to_original_url(key):
 
 @app.route('/create', methods=['POST'])
 def create():
-    global app_data
     data = request.get_json()
 
     is_protected = data['isProtected']
@@ -69,13 +74,11 @@ def create():
         return {'message': 'Please enter a valid URL.'}, 400
 
     key = get_random_unique_key()
-    app_data[key] = {
+    db.set("shortner_" + key, json.dumps({
         'originalUrl': original_url,
         'isProtected': is_protected,
         'password': password
-    }
-
-    json.dump(app_data, open('data.json', 'w'))
+    }))
 
     return {'shortLink': f'{request.host_url}{key}'}
 
